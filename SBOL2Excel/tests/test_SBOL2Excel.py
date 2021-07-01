a1aae823baeeb02bff2449a717196268bdaa2473
JMante1 <40006518+JMante1@users.noreply.github.com>
2021-07-01 19:55:12 +0200

    test rewriting

--- SBOL2Excel/tests/test_SBOL2Excel.py
+++ SBOL2Excel/tests/test_SBOL2Excel.py
@@ -1,35 +1,46 @@
-#pytest -v -s
-
-import SBOL2Excel as s_e
+# pytest -v -s
+import utils.sbol2excel as s_e
 import pytest
-from pandas.testing import assert_frame_equal
-import sbol2
-import pandas as pd 
 import os
-from openpyxl import Workbook, load_workbook
-from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.styles import Font, PatternFill, Border, Side
-from requests_html import HTMLSession
 
 file_dir = os.path.dirname(__file__)
 test_files_path = os.path.join(file_dir, 'test_files')
 
 
-class seqFile:
-    def __init__(self, document):
-        #global varibales for homespace, document, and sheet
-        self.homeSpace = 'http://sys-bio.org'
-        self.document = document
-        self.sheet = 'ontologies.xlsx'
+@pytest.mark.parametrize(
+    'prop, raising_err, expected', [
+        ("http://sbols.org/v2#type", False, 'types'),
+        ("http://purl.org/dc/terms/title", False, 'title'),
+        ("http://purl.obolibrary.org/obo/OBI_0001617", False, 'OBI_0001617'),
+        ("https://wiki.synbiohub.org/wiki/Terms/synbiohub#sourceOrganism", False, 'sourceOrganism'),
+        ("http://purl.org/dc/terms/description", False, 'description'),
+        (76868, True, ValueError)
+    ]
+)
+def test_seqFile_prop_convert(prop, raising_err, expected):
+    seq_file_obj = s_e.seqFile('file_path_in', 'output_path')
+    if raising_err:
+        with pytest.raises(expected):
+            seq_file_obj.prop_convert(prop)
+    else:
+        assert seq_file_obj.prop_convert(prop) == expected
 
-def test_seqClass():
-    x = s_e.seqFile('pichia_toolkit_KWK_v002.xml')
-    assert x.document == 'pichia_toolkit_KWK_v002.xml'
-    assert x.sheet == 'ontologies.xlsx'
-    assert x.homeSpace == 'http://sys-bio.org'
-    
-def test_roleVariables():
-    x = s_e.seqFile('pichia_toolkit_KWK_v002.xml')
-    roleDictionary = x.roleVariables()
-    assert roleDictionary == {}
+# class seqFile:
+#     def __init__(self, document):
+#         # global varibales for homespace, document, and sheet
+#         self.homeSpace = 'http://sys-bio.org'
+#         self.document = document
+#         self.sheet = 'ontologies.xlsx'
+
+
+# def test_seqClass():
+#     x = s_e.seqFile(file_path_in, output_path)
+#     assert x.document == file_path_expected
+#     # assert x.sheet == 'ontologies.xlsx'
+#     # assert x.homeSpace == 'http://sys-bio.org'
+
+
+# def test_roleVariables():
+#     x = s_e.seqFile('pichia_toolkit_KWK_v002.xml')
+#     roleDictionary = x.roleVariables()
+#     assert roleDictionary == {}
