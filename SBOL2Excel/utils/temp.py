5d5fe41e08f8db4c926cadd939fca717f24eb75b
JMante1 <40006518+JMante1@users.noreply.github.com>
2021-07-08 19:05:04 +0200

    working on testing rearchitecting sbol2excel functions

--- SBOL2Excel/utils/temp.py
+++ SBOL2Excel/utils/temp.py
@@ -1,10 +1,18 @@
 import sbol2excel as sb
 import os
+import pandas as pd
 
 cwd = os.getcwd()
 filepathin = os.path.join(cwd, 'SBOL2Excel','tests', 'test_files', 'pichia_toolkit_KWK_v002.xml')
 
 print(filepathin)
 
-sb_obj = sb.seqFile(filepathin, 'output_path')
-sb_obj.readDocChart()
+d = {'col1': [1, 2], 'col2': [3, 4], 'Role':[5,6], 'Design Notes':[7,8]}
+df = pd.DataFrame(data=d)
+
+# print(df)
+
+sb_obj = sb.seqFile(filepathin, 'output_path') # calling __init__
+sb_obj.overarching()
+# print(sb_obj.reorder_columns(df))
+
