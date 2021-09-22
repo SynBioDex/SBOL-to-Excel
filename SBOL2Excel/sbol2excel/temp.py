70e15b19fb4ee57d4207c74b2d3bbf28fcfe70de
abamaj <abamaj@gmail.com>
2021-09-22 10:49:55 -0600

    Update temp.py
    Linting fixes

--- SBOL2Excel/sbol2excel/temp.py
+++ SBOL2Excel/sbol2excel/temp.py
@@ -1,3 +1,8 @@
+"""
+This module serves as a caller to the others.
+
+One the functions are called, your results will be properly output.
+"""
 import sbol2excel.converter as conv
 import os
 import requests
@@ -7,30 +12,38 @@ cwd = os.getcwd()
 
 def callTest(cwd):
     """
-    To test the fuctionality of the sbol2excel converter, run this funtion with
-    the given SBOL file. The 'cwd' parameter has been given to you at the top.
+    Run this function to test the converter.
+
+    The 'cwd' parameter has been given to you at the top.
     """
     sbol_doc_path = os.path.join(
-        cwd,
-        'SBOL2Excel',
-        'tests',
-        'test_files',
-        'test_sbol.xml')
+                                cwd,
+                                'SBOL2Excel',
+                                'tests',
+                                'test_files',
+                                'test_sbol.xml'
+                                )
     output_path = os.path.join(cwd, 'test.xlsx')
     conv.converter(sbol_doc_path, output_path)
 
 
 def callURI(URI, cwd):
     """
-    To convert your own SBOL file into and Excel spreadsheet, insert your own
-    URI as the first parameter. The 'cwd' parameter has been given to you at
-    the top.
+    Use this function to apply the converter to your own SBOL document.
+
+    Your desired URI serves as the first parameter.
+
+    The 'cwd' parameter has been given to you at the top.
     """
     new_xml = 'myFile.xml'
     imported_xml = requests.get(URI)
-    output_path_new_xml = os.path.join(cwd, 'SBOL2Excel', 'tests',
-                                            'test_files',
-                                            new_xml)
+    output_path_new_xml = os.path.join(
+                                        cwd,
+                                        'SBOL2Excel',
+                                        'tests',
+                                        'test_files',
+                                        new_xml
+                                        )
     file = open(output_path_new_xml, "w")
     file.write(imported_xml.text)
     file.close()
@@ -38,7 +51,8 @@ def callURI(URI, cwd):
                                         cwd,
                                         'SBOL2Excel', 'tests',
                                         'test_files',
-                                        new_xml)
+                                        new_xml
+                                        )
 
     output_path_excel = os.path.join(cwd, 'my_xml_to_excel.xlsx')
     conv.converter(sbol_doc_path_new_xml, output_path_excel)
